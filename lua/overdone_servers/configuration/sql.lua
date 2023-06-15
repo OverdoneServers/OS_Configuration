3da107c0dcd092e36a0528c1d10e09efe3c99cf5
Ryan Ernest <ryan@rivergroup.com>
2023-06-15 00:38:21 -0400

    Added support for a SQL connection timeout
    This stops the server from booting until the connection has been made OR the connection timeout ran out.

---
+++ lua/overdone_servers/configuration/sql.lua
@@ -0,0 +1,26 @@
+return {
+
+// #    MySQL
+// #
+// #    Certain functions may require connection to a database inorder to save data.
+// #
+// #    Hostname          : The Host IP or URL of the SQL Server
+// #    Database          : The Database name of the SQL Server
+// #    Username          : The Username of the SQL Server
+// #    Password          : The Password of the SQL Server
+// #    Port              : The Port of the SQL Server
+// #    ConnectionTimeout : How long the server will wait for a connection to
+// #                            the SQL Server before continuing to boot
+
+-- START OF CONFIG --
+
+Enabled = false,
+Hostname = "",
+Database = "",
+Username = "",
+Password = "",
+Port = 3306,
+ConnectionTimeout = 3
+
+-- END OF CONFIG --
+}
